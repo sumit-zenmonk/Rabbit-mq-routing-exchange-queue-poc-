7c0d47a95e5f3b069179f3ab7d801852eac63bc8
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-05-07 11:44:10 +0530

    validator + rabbitmq enum included

---
+++ microservice-1/src/domain/rabbit-mq/enum/rabbit-mq.enum.ts
@@ -0,0 +1,11 @@
+export enum ExchangeTypeEnum {
+    DIRECT = 'direct',
+    FANOUT = 'fanout',
+    TOPIC = 'topic',
+    HEADERS = 'headers',
+}
+
+export enum XMatchHeaderEnum {
+    ALL = 'all',
+    ANY = 'any'
+}
