18f2bdab8be4e712b74e20e6fb1b2c32fff44b48
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-05-07 17:15:27 +0530

    microservice-2 implemented

--- microservice-2/src/app.controller.ts
+++ microservice-2/src/app.controller.ts
@@ -1,6 +1,8 @@
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Micro2RabbitMQService } from './infrastructure/rabbit-mq/rabbit-mq.service';
+import type { Request } from 'express';
+import { ExchangeTypeEnum, XMatchHeaderEnum } from './domain/rabbit-mq/enum/rabbit-mq.enum';
 
 @Controller()
 export class AppController {
@@ -14,10 +16,36 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post("send")
-  async sendMessage(@Body() payload: { message: string }) {
-    await this.micro2RabbitMQService.sendMessage(payload.message);
-    return { status: "Message sent successfully" };
+  // direct exchange api
+  @Post("direct")
+  async send(@Body() body: any) {
+    await this.micro2RabbitMQService.publishToExchange('direct_exchange', body.key || 'micro1', body);
+    return { status: "Message sent (direct)" };
   }
 
+  // fanout exchange api
+  @Post("broadcast")
+  async broadcast(@Body() body: any) {
+    await this.micro2RabbitMQService.publishToExchange('fanout_exchange', '', body);
+    return { status: "Broadcast sent" };
+  }
+
+  // topic exchange api
+  @Post("topic")
+  async topic(@Body() body: any) {
+    if (!body.key) {
+      throw new BadRequestException("key required")
+    }
+
+    await this.micro2RabbitMQService.publishToExchange('topic_exchange', body.key, body);
+    return { status: "Topic message sent" };
+  }
+
+  // headers exchange api
+  @Post("headers")
+  async headers(@Req() req: Request, @Body() body: any) {
+    const headers = { type: req.headers.type, topic: req.headers.topic };
+    await this.micro2RabbitMQService.publishToExchange('headers_exchange', '', body, { "x-match": XMatchHeaderEnum.ALL, ...headers });
+    return { status: "headers message sent" };
+  }
 }
